e1044e131acc2a3eaa7eb55e9b802e70116d0303
刘超 <siyuekuangke@163.com>
2023-04-25 15:59:44 +0800

    sql

--- src/Providers/DatabaseProvider.php
+++ src/Providers/DatabaseProvider.php
@@ -4,6 +4,7 @@ namespace Healthplat\Tool\Providers;
 
 use Healthplat\Tool\Mysql;
 use Phalcon\Config\Config;
+use Phalcon\Di\Di;
 use Phalcon\Di\ServiceProviderInterface;
 use Phalcon\DiInterface;
 use Phalcon\Events\Manager;
@@ -83,6 +84,24 @@ class DatabaseProvider implements ServiceProviderInterface
                     }
                     if ($event->getType() == 'afterQuery') {
                         $profiler->stopProfile();
+                        $requestId = Di::getDefault()->get('serviceServer')->getPhalconResponse()->getHeaders()->get('X-REQUEST-Id');
+                        $profiler = Di::getDefault()->getProfiler();
+                        // 获取所有记录的查询
+                        $profiles = $profiler->getProfiles();
+                        foreach ($profiles as $profile) {
+                            // 获取查询的 SQL 语句、参数和绑定类型
+                            $sql = $profile->getSQLStatement();
+                            $params = $profile->getSQLVariables();
+                            // 获取查询的执行时间
+                            $time = $profile->getTotalElapsedSeconds();
+                            if ($time > 1) {
+                                // 将查询写入日志文件
+                                Di::getDefault()->getLogger('app')->info(sprintf('请求结束,请求链[' . $requestId . '], 请求超时- %s (%s s): %s', $sql, $time, json_encode($params)));
+                            } else {
+                                // 将查询写入日志文件
+                                Di::getDefault()->getLogger('app')->info(sprintf('请求结束,请求链[' . $requestId . '], %s (%s s): %s', $sql, $time, json_encode($params)));
+                            }
+                        }
                     }
                 });
                 $db->setEventsManager($eventsManager);
--- src/Providers/ProfilerProvider.php
+++ src/Providers/ProfilerProvider.php
@@ -2,7 +2,6 @@
 
 namespace Healthplat\Tool\Providers;
 
-use Phalcon\Db\Profiler;
 use Phalcon\Di\ServiceProviderInterface;
 
 class ProfilerProvider implements ServiceProviderInterface
@@ -10,7 +9,7 @@ class ProfilerProvider implements ServiceProviderInterface
     public function register(\Phalcon\Di\DiInterface $di): void
     {
         $di->getShared('profiler',function (){
-            return new Profiler();
+            return new \Phalcon\Db\Profiler();
         });
 
     }
